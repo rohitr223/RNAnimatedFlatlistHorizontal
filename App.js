4b22f125ab484c8b6e5203a5f88735eee41f6278
rohit <rohitran236@gmail.com>
2023-08-04 09:37:27 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  Text,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { DATA } from "./data";
 
-export default function App() {
+const App = () => {
+  // get the window dimensions
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+ 
+  // adjusting the screen height & width according to our requirements
+  const screenWidth = width * 0.7;
+  const screenHeight = height * 0.5;
+
+  const scrollX = useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <StatusBar color="#fff" />
+      <Animated.FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        decelerationRate={0}
+        snapToInterval={screenWidth}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => {
+          // using animation for Flatlist
+          const inputRange = [
+            (index - 1) * screenWidth,
+            index * screenWidth,
+            (index + 1) * screenWidth,
+          ];
+
+          const outputRange = [0, -50, 0];
+
+          const translateY = scrollX.interpolate({
+            inputRange,
+            outputRange,
+          });
+
+          return (
+            <Animated.View
+              style={[
+                styles.cardContainer,
+                { backgroundColor: item.color, width: screenWidth, height: screenHeight,transform: [{translateY}] },
+              ]}
+            >
+              <Text style={styles.title}>{item.title}</Text>
+            </Animated.View>
+          );
+        }}
+        contentContainerStyle={styles.flatList}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#252525",
+  },
+  flatList: {
+    alignItems: "center",
+  },
+  cardContainer: {
+    borderWidth: 10,
+    borderColor: '#EFEFEF',
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    margin: 10,
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 22,
+    fontWeight: "700",
   },
 });
---
+++ data.js
@@ -0,0 +1,52 @@
+export const DATA = [
+    {
+        id: 1,
+        title: 'First',
+        color: '#35155D',
+    },
+    {
+        id: 2,
+        title: 'Second',
+        color: '#512B81',
+    },
+    {
+        id: 3,
+        title: 'Third',
+        color: '#4477CE',
+    },
+    {
+        id: 4,
+        title: 'Fourth',
+        color: '#8CABFF',
+    },
+    {
+        id: 5,
+        title: 'Fifth',
+        color: '#F11A7B',
+    },
+    {
+        id: 6,
+        title: 'Sixth',
+        color: '#982176',
+    },
+    {
+        id: 7,
+        title: 'Seventh',
+        color: '#0D1282',
+    },
+    {
+        id: 8,
+        title: 'Eight',
+        color: '#D71313',
+    },
+    {
+        id: 9,
+        title: 'Ninth',
+        color: '#557A46',
+    },
+    {
+        id: 10,
+        title: 'Tenth',
+        color: '#6528F7',
+    },
+];
